5f78a3ff554faf7529726c63446c2253d0fd77b0
Ronan Grenet <ronan.grenet@laposte.net>
2018-04-19 14:00:46 +0200

    classe my

--- command/classe.js
+++ command/classe.js
@@ -1,31 +1,38 @@
 exports.classe = function classe(param, roles) {
 		
-	var s = '';
+	let s = '';
 	args.forEach(function(element) {
-		if(element === 'guerrier')
-			s += guerrierA;
-		if(element === 'magicien')
-			s += magicienA;
-		if(element === 'archer')
-			s += archerA;
-		if(element === 'voleur')
-			s += voleurA;
-		if(element === 'chaman')
-			s += chamanA;
-		if(element === 'amazone')
-			s += amazoneA;
 		if(element === 'my') {
-			roles.forEach(function(element) {
-				console.log(element.name);
+			roles.forEach(function(role) {
+				s+= getInfo(role);
 			});
 			
-			s+= 'my'
+			
 		}
-		if(element === 'all') {
+		else if(element === 'all') {
 			s += guerrierE + 'Guerrier \n' + magicienE + 'Magicien \n' + voleurE + 'Voleur \n' + archerE + 'Archer \n' + chamanE + 'Chaman \n' + amazoneE +'Amazone';
 		}
-			
+		else {
+			s += getInfo(element);
+		}
 	});
 	
 	return s
+}
+
+function getInfo(name) {
+	let s = '';
+	if(element === 'guerrier')
+		s += guerrierA;
+	if(element === 'magicien')
+		s += magicienA;
+	if(element === 'archer')
+		s += archerA;
+	if(element === 'voleur')
+		s += voleurA;
+	if(element === 'chaman')
+		s += chamanA;
+	if(element === 'amazone')
+		s += amazoneA;
+	return s;
 }
