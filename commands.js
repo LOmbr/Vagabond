8064184b5822768c72ce00de48963cc3fe9c0ef3
Ronan Grenet <ronan.grenet@laposte.net>
2018-04-19 14:49:46 +0200

    debug heroku

--- commands.js
+++ commands.js
@@ -15,39 +15,39 @@ exports.commands = function commands(msg, members, mapUser) {
 		s += index.index();
 		msg.channel.send(s);
 	}
-	if (msg.content === '!v download') {
+	else if (msg.content === '!v download') {
 		msg.channel.send(dl.download());
 	}
-	if (msg.content === '!v wiki') {
+	else if (msg.content === '!v wiki') {
 		msg.channel.send(wiki.wiki());
 	}
-	if (msg.content.startsWith('!v classe') && msg.content !== '!v classes') {
+	else if (msg.content.startsWith('!v classe') && msg.content !== '!v classes') {
 		modif = msg.content.replace('!v classe', '');
 		args = modif.split(' ').slice(1);
 		msg.channel.send(classe.classe(args, msg.member.roles));
 	}
-	if (msg.content === '!v emojis') {
+	else if (msg.content === '!v emojis') {
 		msg.channel.send(emojis.emojis(msg));
 	}
-	if (msg.content === '!v evolve') {
+	else if (msg.content === '!v evolve') {
 		msg.channel.send(evolve.evolve());
 	}
-	if (msg.content.startsWith('!v all')) {
+	else if (msg.content.startsWith('!v all')) {
 		modif = msg.content.replace('!v all', '');
 		args = modif.split(' ').slice(1);
 		msg.channel.send(alls.alls(mapUser, args));
 	}
-	if (msg.content === '!v say') {
+	else if (msg.content === '!v say') {
 		msg.channel.send(say.say());
 	}
-	if (msg.content === '!v images') {
+	else if (msg.content === '!v images') {
 		msg.channel.send("", {file: imag.imag()});
+	}
+	else if (msg.content === '!v github') {
+		msg.channel.send(github.github());
 	}
 	if (msg.content.indexOf(':ersil:') > -1) {
 		msg.channel.send(ersil.ersil());
-	}
-	if (msg.content === '!v github') {
-		msg.channel.send(github.github());
 	}
 
 }
