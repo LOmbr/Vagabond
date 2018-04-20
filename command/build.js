ad10b166a449584466982d1f06ad6e3394ab4122
Ronan Grenet <ronan.grenet@laposte.net>
2018-04-20 14:58:10 +0200

    build usage

--- command/build.js
+++ command/build.js
@@ -1,12 +1,20 @@
 exports.build = function build(args, mapBuilds) {
 	
-	s = args[0];
-	if(args.length > 1) {
-		s += '_' + args[1];
+	if(args.length > 0) {
+		s = args[0];
+		if(args.length > 1) {
+			s += '_' + args[1];
+		}
+		s+='.PNG';
+		
+		//return s;
+		return __dirname+'/build/'+ mapBuilds.get(s);
 	}
-	s+='.PNG';
-	
-	//return s;
-	return __dirname+'/build/'+ mapBuilds.get(s);
+	else {
+		s = 'Utilisation : \n';
+		s += '```!v build all ```\n';
+		s += 'ou ```!v build nom_de_la_classe ```\n';
+		s += 'ou ```!v build nom_de_la_classe specifiation```\n';
+	}
 }
 
