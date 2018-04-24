90e01fb33252493e9691a22d12c1c8e66785342d
Ronan Grenet <ronan.grenet@laposte.net>
2018-04-24 09:44:48 +0200

    debug double affichage

--- bot.js
+++ bot.js
@@ -22,16 +22,6 @@ client.on('message', msg => {
 	if(msg.content.startsWith(prefixe) || msg.content.indexOf(':pororing:') > -1 || msg.content.indexOf(':ersil:') > -1) {
 		commands.commands(msg, members, mapUser, mapBuilds);
 	}
-	else if(msg.content.startsWith('!roles')) {
-		
-		s = '';
-		w = '';
-		msg.member.roles.forEach(function(element) {
-			s+= element + ' ';
-			w+= element.toString().replace('@', '');
-		});
-		msg.channel.send(s + '\n' + w);
-	}
 });
 
 client.login(process.env.TOKEN);
