148bdafc5784853a96f8b24cc5063e350d8f27ce
Ronan Grenet <ronan.grenet@laposte.net>
2018-04-24 15:02:51 +0200

    test wanted

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ client.on('message', msg => {
 	
 	if(msg.content.startsWith(prefixe) || msg.content.indexOf(':pororing:') > -1 || msg.content.indexOf(':ersil:') > -1) {
 		commands.commands(msg, members, mapUser, mapBuilds);
-		console.log(msg);
+		console.log(msg['user']);
 		
 	}
 });
