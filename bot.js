00ce58a1f0da61452432645f2d58bd964696d01c
Ronan Grenet <ronan.grenet@laposte.net>
2018-04-24 15:21:23 +0200

    test wanted

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ client.on('message', msg => {
 	
 	if(msg.content.startsWith(prefixe) || msg.content.indexOf(':pororing:') > -1 || msg.content.indexOf(':ersil:') > -1) {
 		commands.commands(msg, members, mapUser, mapBuilds);
-		console.log(members);
+		
 		//console.log(msg.author);
 		/*client.fetchUser(msg.author).then(myUser => {
 			console.log(myUser.avatarURL); // My user's avatar is here!
