f446675938a31647340e8710f4c243fffac3513a
Ronan Grenet <ronan.grenet@laposte.net>
2018-04-24 15:45:02 +0200

    wanted step 1

--- command/wanted.js
+++ command/wanted.js
@@ -1,6 +1,6 @@
 exports.wanted = function wanted(tmp, client) {
 		client.fetchUser(tmp).then(myUser => {
-			console.log(myUser.avatarURL.split('?')[0]);
-			return myUser.avatarURL; // My user's avatar is here!
+			ret = myUser.avatarURL.split('?')[0];
+			return ret;
 		});
 }
