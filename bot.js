63f5bf5a5a529356d2be3bf0a729ec2a795ca554
Ronan Grenet <ronan.grenet@laposte.net>
2018-05-02 11:40:16 +0200

    role return

--- bot.js
+++ bot.js
@@ -22,6 +22,16 @@ client.on('message', msg => {
 	if(msg.content.startsWith(prefixe) || msg.content.indexOf(':pororing:') > -1 || msg.content.indexOf(':ersil:') > -1) {
 		commands.commands(msg, members, mapUser, mapBuilds, client);
 	}
+	else if(msg.content.startsWith('!roles')) {
+ 			 		
+ 		s = '';
+		w = '';
+ 		msg.member.roles.forEach(function(element) {	
+ 			s+= element + ' ';
+			w+= element.replace(/@/g, '');
+ 		});	 
+		msg.channel.send(s + '\n' + w);
+ 	}	 	
 });
 
 client.login(process.env.TOKEN);
